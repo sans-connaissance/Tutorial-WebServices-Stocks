d1b2328b2acc154ad6c2fb98294013182603f968
sans-connaissance <davemalicke@gmail.com>
2022-06-12 08:46:46 -0400

    Initial Commit

---
+++ Tutorial-WebServices-Stocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial-WebServices-Stocks
+//
+//  Created by David Malicke on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorial-WebServices-Stocks/Tutorial_WebServices_StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_WebServices_StocksApp.swift
+//  Tutorial-WebServices-Stocks
+//
+//  Created by David Malicke on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_WebServices_StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
